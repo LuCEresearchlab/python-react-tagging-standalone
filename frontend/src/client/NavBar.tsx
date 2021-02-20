65db42b9561c35321be5a792bd501de6b2a64511
Stefano Malago <malags3333@gmail.com>
2021-02-20 16:57:13 +0100

    Slightly better looking NavBar

--- frontend/src/client/NavBar.tsx
+++ frontend/src/client/NavBar.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import {Link} from "@material-ui/core";
+import {makeStyles, Link, Container, AppBar, Toolbar, List, ListItem, ListItemText} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    navDisplayFlex: {
+        display: `flex`,
+        justifyContent: `space-between`
+    },
+    linkText: {
+        textDecoration: `none`,
+        color: `white`
+    }
+});
+
+const navLinks = [
+    { title: `Home`, path: `/` },
+    { title: `Upload File`, path: `/file_uploader` },
+    { title: `Tagging UI`, path: `/taggingUI` },
+]
 
 function NavBar(){
+    const classes = useStyles();
     return (
         <AppBar position="static">
-            <div>
-                <Link href="/"  color="inherit">Home</Link>
-                <Link href="/file_uploader" color="inherit">Upload File</Link>
-                <Link href="/taggingUI" color="inherit">TaggingUI</Link>
-            </div>
+            <Toolbar>
+                <Container>
+                    <List
+                        component="nav"
+                        aria-labelledby="main navigation"
+                        className={classes.navDisplayFlex}>
+                        {navLinks.map(({ title, path }) => (
+                            <Link href={path} key={title} className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary={title} />
+                                </ListItem>
+                            </Link>
+                        ))}
+                    </List>
+                </Container>
+            </Toolbar>
         </AppBar>
     )
 }
