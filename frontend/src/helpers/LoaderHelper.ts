0feba0af33e3cc25a9c5b9f0e8c529c4439a8eab
Stefano Malagò <malags@usi.ch>
2021-04-10 13:49:44 +0200

    Changed useFetch and some usage

--- frontend/src/helpers/LoaderHelper.ts
+++ frontend/src/helpers/LoaderHelper.ts
@@ -8,8 +8,13 @@ export function JSONLoader(url: string, next: Function): void {
         })
 }
 
-export function useFetch(url: string): { data: any, isLoading: boolean } {
-    const [state, setState] = useState<any>({data: null, isLoading: true});
+interface useFetchInterface<T> {
+    data: T,
+    isLoading: boolean
+}
+
+export function useFetch<T>(url: string): useFetchInterface<T> {
+    const [state, setState] = useState<{ data: any, isLoading: boolean }>({data: null, isLoading: true});
     const isMounted = useRef<boolean>(true);
 
     useEffect(() => {
@@ -19,11 +24,13 @@ export function useFetch(url: string): { data: any, isLoading: boolean } {
     }, [])
 
     useEffect(() => {
-        setState({...state, data: state.data, loading: true})
         fetch(url)
             .then(response => response.json())
             .then(json => {
-                if (isMounted.current) setState({...state, data: json, loading: false})
+                if (isMounted.current) {
+                    console.log({...state, data: json, isLoading: false})
+                    setState({...state, data: json, isLoading: false})
+                }
             })
     }, [url, setState]);
     return state;
--- frontend/src/pages/tagging/TaggingPage.tsx
+++ frontend/src/pages/tagging/TaggingPage.tsx
@@ -1,29 +1,25 @@
 import React, {useState} from 'react';
 import {Container} from "@material-ui/core";
-import {JSONLoader} from '../../helpers/LoaderHelper';
+import {useFetch} from '../../helpers/LoaderHelper';
 import {useParams} from "react-router-dom";
-import {Dataset} from '../../interfaces/Dataset'
 import TaggingUI from '../../components/v2/TaggingUI'
 import TaggingSession from "../../model/TaggingSession";
+import {Dataset} from "../../interfaces/Dataset";
 
 const {TAGGING_SERVICE_URL} = require('../../../config.json')
 
 function TaggingPage() {
     const [taggingSession, setTaggingSession] = useState<TaggingSession | undefined>(undefined)
-    const [loaded, setLoaded] = useState<boolean>(false)
 
     const {dataset_id, user_id}: { dataset_id: string, user_id: string } = useParams()
 
     const [key, setKey] = useState<number>(0)
 
-    const url: string = TAGGING_SERVICE_URL + '/datasets/get-dataset/dataset/' + dataset_id
+    const {data, isLoading} = useFetch<Dataset>(`${TAGGING_SERVICE_URL}/datasets/get-dataset/dataset/${dataset_id}`)
 
 
-    if (!loaded) {
-        JSONLoader(url, (data: Dataset) => {
-            setTaggingSession(new TaggingSession(data, user_id, setKey))
-        })
-        setLoaded(true)
+    if (!isLoading) {
+        setTaggingSession(new TaggingSession(data, user_id, setKey))
     }
 
 
