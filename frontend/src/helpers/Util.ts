6002ff876959ac8b385f9fbb9988d8bd24edc5d0
Stefano Malagò <malags@usi.ch>
2021-04-22 12:13:31 +0200

    Changed color handling for highlighting

--- frontend/src/helpers/Util.ts
+++ frontend/src/helpers/Util.ts
@@ -56,6 +56,29 @@ function getColor(misconceptions_available: MisconceptionElement[], misc: (strin
     return found ? found.color : ""
 }
 
+function pickTextColorBasedOnBgColorAdvanced(backgroundColor: string): string {
+    const color = backgroundColor.substring(1, 7)
+    const r = parseInt(color.substring(0, 2), 16); // hexToR
+    const g = parseInt(color.substring(2, 4), 16); // hexToG
+    const b = parseInt(color.substring(4, 6), 16); // hexToB
+    const uicolors = [r / 255, g / 255, b / 255];
+    const c = uicolors.map((col: number) => {
+        if (col <= 0.03928) {
+            return col / 12.92;
+        }
+        return Math.pow((col + 0.055) / 1.055, 2.4)
+    })
+    const L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2])
+    return (L > 0.179) ? '#000000' : '#FFFFFF'
+}
+
+function highlightStyle(range: HighlightRange) {
+    return {
+        color: pickTextColorBasedOnBgColorAdvanced(range.color),
+        backgroundColor: range.color,
+    }
+}
+
 export {
     NO_COLOR,
     isUsingDefaultColor,
@@ -63,5 +86,6 @@ export {
     getMillis,
     highlightRangesColorUpdating,
     filteredMisconceptions,
-    getColor
+    getColor,
+    highlightStyle
 }
