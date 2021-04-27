c196f5800141834e45b0a0d9f7cd4f9c363764d1
Stefano Malagò <malags@usi.ch>
2021-04-27 23:05:47 +0200

    Added data for experiment

---
+++ scripts/experiments/clustering/metric_compute.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import json
+import os
+import numpy as np
+from sklearn.metrics import silhouette_samples
+
+from model_tests import get_similarity_matrix, clean_answers_only_lowercase, \
+    clean_answers_only_stopwords, clean_answers_only_lemma, clean_answers_full, clean_answers_stopwords_lemma, raw
+
+
+def get_key(x):
+    return x['idx']
+
+
+def load_cluster():
+    for root, directories, files in os.walk('./data'):
+        for file in sorted(files):
+            if file.startswith("experiment") and file.endswith(".json"):
+                file_name = os.path.join(root, file)
+                with open(file_name, 'r') as f:
+                    clustering = json.loads(f.read())
+                    compute_metrics(clustering, file_name)
+
+
+def compute_metrics(clusters, file_name):
+    is_lower_case = '_only_lowercase' in file_name
+    is_stopwords = '_only_stopwords' in file_name
+    is_lemma = '_only_lemma' in file_name
+    is_full = '_full' in file_name
+    is_raw = '_raw' in file_name
+    is_stopwords_lemma = '_stopwords_lemma' in file_name
+
+    answers = []
+    for cluster in clusters:
+        for answer in cluster:
+            answers.append(answer)
+
+    answers.sort(key=get_key)
+    min_idx = np.min([a['idx'] for a in answers])  # adjust with min index to shift to [0, len-1]
+
+    clean_func = None
+    if is_lower_case:
+        clean_func = clean_answers_only_lowercase
+    elif is_stopwords:
+        clean_func = clean_answers_only_stopwords
+    elif is_lemma:
+        clean_func = clean_answers_only_lemma
+    elif is_full:
+        clean_func = clean_answers_full
+    elif is_raw:
+        clean_func = raw
+    elif is_stopwords_lemma:
+        clean_func = clean_answers_stopwords_lemma
+    else:
+        exit(1)
+
+    similarity_matrix = get_similarity_matrix(answers=answers, clean_function=clean_func)
+
+    def set_diagonal_0():
+        for i in range(len(answers)):
+            similarity_matrix[i][i] = 0  #
+
+    set_diagonal_0()
+    maximal_value = np.max(similarity_matrix)
+    similarity_matrix = similarity_matrix / maximal_value  # normalize
+    similarity_matrix = 1 - similarity_matrix
+    set_diagonal_0()
+
+    # some questions have missing elements due to lack of explanation
+    labels = [-1 for _ in range(33)]
+    for cluster_idx, cluster in enumerate(clusters):
+        for answer in cluster:
+            labels[answer['idx'] - min_idx] = cluster_idx
+
+    labels = list(filter(lambda x: x != -1, labels))
+
+    distance_matrix = similarity_matrix  # normalized and inverted
+
+    silhouette_score = silhouette_samples(distance_matrix, labels=labels, metric='precomputed')
+
+    print(file_name)
+    print(clusters)
+    print('silhouette scores')
+    print(silhouette_score)
+    print(f'max: {np.max(silhouette_score)} arg: {np.argmax(silhouette_score)}')
+    print(f'mean: {np.mean(silhouette_score)}')
+    print(f'min: {np.min(silhouette_score)} arg: {np.argmin(silhouette_score)}')
+    print(f'std: {np.std(silhouette_score)}')
+    print('\n')
+
+
+load_cluster()
---
+++ scripts/experiments/clustering/model_tests.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+import json
+import pathlib
+
+import math
+import numpy as np
+import nltk
+
+from semantic_text_similarity.models import WebBertSimilarity
+from sklearn.cluster import SpectralClustering
+
+from nltk.corpus import stopwords, wordnet
+from nltk.tokenize import word_tokenize
+
+
+def load_answers(question_nr):
+    file = pathlib.Path("../../Anonymized-PS2020-Midterm-Results.json")
+
+    with open(file, 'r') as file:
+        content = file.read()
+        j = json.loads(content)
+        question = j['questions'][question_nr]
+        return question['answers']
+
+
+web_model = WebBertSimilarity(device='cpu', batch_size=32)
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words('english'))
+lemma = nltk.wordnet.WordNetLemmatizer()
+wordnet.ensure_loaded()
+
+
+def raw(answers):
+    return [answer['data'] for answer in answers]
+
+
+def clean_answers_full(answers):
+    cleaned_answers = []
+
+    for answer in answers:
+        word_tokens = word_tokenize(answer['data'], language='english')  # tokenize
+        filtered_sentence = [w.lower() for w in word_tokens if w not in stop_words]  # remove stopwords and lowercase
+        lemmatized = [lemma.lemmatize(w) for w in filtered_sentence]  # lemmatize
+        cleaned_answers.append(' '.join(lemmatized))  # join back
+    return cleaned_answers
+
+
+def clean_answers_stopwords_lemma(answers):
+    cleaned_answers = []
+
+    for answer in answers:
+        word_tokens = word_tokenize(answer['data'], language='english')  # tokenize
+        filtered_sentence = [w for w in word_tokens if w not in stop_words]  # remove stopwords
+        lemmatized = [lemma.lemmatize(w) for w in filtered_sentence]  # lemmatize
+        cleaned_answers.append(' '.join(lemmatized))  # join back
+    return cleaned_answers
+
+
+def clean_answers_only_lemma(answers):
+    cleaned_answers = []
+
+    for answer in answers:
+        word_tokens = word_tokenize(answer['data'], language='english')  # tokenize
+        lemmatized = [lemma.lemmatize(w) for w in word_tokens]  # lemmatize
+        cleaned_answers.append(' '.join(lemmatized))  # join back
+    return cleaned_answers
+
+
+def clean_answers_only_stopwords(answers):
+    cleaned_answers = []
+
+    for answer in answers:
+        word_tokens = word_tokenize(answer['data'], language='english')  # tokenize
+        filtered_sentence = [w for w in word_tokens if w not in stop_words]  # remove stopwords
+        cleaned_answers.append(' '.join(filtered_sentence))  # join back
+    return cleaned_answers
+
+
+def clean_answers_only_lowercase(answers):
+    cleaned_answers = []
+
+    for answer in answers:
+        cleaned_answers.append(' '.join(answer['data'].lower()))  # join back
+    return cleaned_answers
+
+
+def get_similarity_matrix(answers, clean_function):
+    nr_elements = len(answers)
+    sim_matrix = np.zeros((nr_elements, nr_elements))
+
+    cleaned_answers = clean_function(answers)
+
+    for i, elem1 in enumerate(cleaned_answers):
+        for j, elem2 in enumerate(cleaned_answers):
+            if j >= i:  # only compute half
+                sim_matrix[i][j] = web_model.predict([(elem1, elem2)])
+
+    # fill in bottom left side
+    for i in range(1, nr_elements):
+        for j in range(nr_elements):
+            if j < i:
+                sim_matrix[i][j] = sim_matrix[j][i]
+
+    return sim_matrix
+
+
+def _cluster(answers, clean_func):
+    nr_answers = len(answers)
+    nr_clusters = max(int(math.ceil(nr_answers / 10)), 2)
+
+    sim_matrix = get_similarity_matrix(answers, clean_func)
+
+    clustering = SpectralClustering(n_clusters=nr_clusters,
+                                    affinity='precomputed',
+                                    random_state=0,
+                                    n_init=50).fit(sim_matrix)
+
+    clusters = [[] for _ in range(nr_clusters)]
+    for idx, cluster_idx in enumerate(clustering.labels_):
+        clusters[cluster_idx].append(answers[idx])
+    return clusters
+
+
+def cluster(answers, clean_func):
+    clusters = _cluster(answers, clean_func)
+    final_clusters = []
+    for c in clusters:
+        if len(c) <= 5:
+            final_clusters.append(c)
+        else:
+            final_clusters.extend(cluster(c, clean_func))
+    return final_clusters
+
+
+def compute_metrics(question_idx):
+    dirty_answers = load_answers(question_idx)
+    clean_functions = [raw, clean_answers_only_lowercase, clean_answers_only_stopwords, clean_answers_only_lemma,
+                       clean_answers_stopwords_lemma, clean_answers_full]
+
+    computed_clusters = []
+
+    for clean_function in clean_functions:
+        computed_clusters.append(cluster(dirty_answers, clean_func=clean_function))
+
+    for idx in range(len(clean_functions)):
+        question_idx = str(question_idx)
+        if len(question_idx) < 2:
+            question_idx = '0' + question_idx
+
+        with open(f"./data/experiment_q{question_idx}_{clean_functions[idx].__name__}.json", "w") as file:
+            cs = computed_clusters[idx]
+            file.write('[\n')
+            array_content = []
+            for c in cs:
+                answers_to_write = []
+                for a in c:
+                    answers_to_write.append('\t\t{"data": "' + a['data'].replace('"', '\'') + '", "idx": '
+                                            + str(a['answer_id']) + '}')
+                array_content.append('\t[\n' + ',\n'.join(answers_to_write) + '\n\t]\n')
+            file.write(','.join(array_content))
+
+            file.write(']\n')
+        print(f'done with {clean_functions[idx].__name__}')
+
+
+if __name__ == "__main__":
+    _filepath = pathlib.Path("../../Anonymized-PS2020-Midterm-Results.json")
+
+    nr_questions = 0
+
+    with open(_filepath, 'r') as _file:
+        j = json.loads(_file.read())
+        nr_questions = len(j['questions'])
+
+    for i in range(nr_questions):
+        compute_metrics(i)
